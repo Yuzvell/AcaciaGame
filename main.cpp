1192fe33988e0c9bb31a808e2a9fb57e2c068a05
KazukiIhara <k023g0076@g.neec.ac.jp>
2023-10-23 11:53:10 +0900

    ウィンドウのタイトルを設定

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include "DxLib.h"
 
 // ウィンドウのタイトルに表示する文字列
-const char TITLE[] = "";
+const char TITLE[] = "AcaciaGame";
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine,
 	_In_ int nCmdShow) {
